398dc9aae584e4cc96fb06fbe9712ed6de32cd2d
Angus MacCuish <angus.maccuish@gmail.com>
2015-02-26 21:16:42 +0000

    Tidy up

--- snap.R
+++ snap.R
@@ -23,29 +23,35 @@ pair.probability.with.2.suits <- function(k, ranks=13, with.rogue=FALSE) {
 # Calculate theoretical probability of a pair occurring within the first k cards.
 # Hardcoded for case of suits=3 case.
 pair.probability.with.3.suits <- function(k, ranks=13, with.rogue=FALSE) {
-  dk <- if (with.rogue) 1 else 0
   outer <- function(p) {
     t.min <- max(0, p-ranks)
-    t.max <- floor((k-dk)/3)
+    t.max <- floor(k/3)
     inner <- function(t) {
       q <- (-1)^(p+1)
       q <- q * choose(ranks, p-2*t)
       q <- q * choose(ranks-p+2*t, t)
       q <- q * 6^(p-t)
       q <- q / prod((3*ranks+t-2*p+1):(3*ranks))
-      q <- q * prod((k-dk+t-2*p+1):(k-p))
-      if (with.rogue) q <- q/k
+      q <- q * prod((k+t-2*p+1):(k-p))
+      if (with.rogue) q <- q * (1-(2*p-t)/k)
       return (q)
     }
     return (sum(sapply(t.min:t.max, inner)))
   }
-  p.max <- floor(2*(k-dk)/3)
+  p.max <- floor(2*k/3)
   p <- if (p.max > 0) sum(sapply(1:p.max, outer)) else 0
   if (with.rogue) {
     weight <- k/(3*ranks+1)
     p <- p * weight + pair.probability.with.3.suits(k, ranks, with.rogue=FALSE) * (1-weight)
   }
   return (p)
+}
+
+
+# Calculate theoretical probability of a pair occurring within the first k cards.
+# Hardcoded for case of suits=4 case.
+pair.probability.with.4.suits <- function(k, ranks=13) {
+  return (0.0)
 }
 
 
