e6eb58a117efa5dfdbffb43122edd9ee9674edac
Angus MacCuish <angus.maccuish@gmail.com>
2015-02-28 15:40:58 +0000

    Almost there with 4 suits (no rogues), a few NaNs to fix for certain k values

--- snap.R
+++ snap.R
@@ -23,23 +23,23 @@ pair.probability.with.2.suits <- function(k, ranks=13, with.rogue=FALSE) {
 # Calculate theoretical probability of a pair occurring within the first k cards.
 # Hardcoded for case of suits=3 case.
 pair.probability.with.3.suits <- function(k, ranks=13, with.rogue=FALSE) {
-  outer <- function(p) {
-    t.min <- max(0, p-ranks)
-    t.max <- floor(k/3)
+  outer <- function(n) {
+    t.min <- max(0, n-ranks) # the minimum number of triples
+    t.max <- floor(n/2) # the maximum number of triples
     inner <- function(t) {
-      q <- (-1)^(p+1)
-      q <- q * choose(ranks, p-2*t)
-      q <- q * choose(ranks-p+2*t, t)
-      q <- q * 6^(p-t)
-      q <- q / prod((3*ranks+t-2*p+1):(3*ranks))
-      q <- q * prod((k+t-2*p+1):(k-p))
-      if (with.rogue) q <- q * (1-(2*p-t)/k)
+      q <- (-1)^(n+1)
+      q <- q * choose(ranks, n-2*t)
+      q <- q * choose(ranks-n+2*t, t)
+      q <- q * 6^(n-t)
+      q <- q / prod((3*ranks+t-2*n+1):(3*ranks))
+      q <- q * prod((k+t-2*n+1):(k-n))
+      if (with.rogue) q <- q * (1-(2*n-t)/k)
       return (q)
     }
     return (sum(sapply(t.min:t.max, inner)))
   }
-  p.max <- floor(2*k/3)
-  p <- if (p.max > 0) sum(sapply(1:p.max, outer)) else 0
+  n.max <- floor(2*k/3)
+  p <- if (n.max > 0) sum(sapply(1:n.max, outer)) else 0
   if (with.rogue) {
     weight <- k/(3*ranks+1)
     p <- p * weight + pair.probability.with.3.suits(k, ranks, with.rogue=FALSE) * (1-weight)
@@ -51,7 +51,40 @@ pair.probability.with.3.suits <- function(k, ranks=13, with.rogue=FALSE) {
 # Calculate theoretical probability of a pair occurring within the first k cards.
 # Hardcoded for case of suits=4 case.
 pair.probability.with.4.suits <- function(k, ranks=13) {
-  return (0.0)
+  outer <- function(n) {
+    q.min <- max(0, n-floor(k/2)) # the minimum number of quartets
+    q.max <- floor(n/3) # the maximum number of quartets
+    innerq <- function(q) {
+      d.min <- max(0, n-floor(2*k/3)-3*q) # the minimum number of doublets
+      d.max <- floor((n-3*q)/2) # the maximum number of doublets
+      innerd <- function(d) {
+        t.min <- max(0, n-floor(2*k/3)-3*q-2*d) # the minimum number of triplets
+        t.max <- floor((n-3*q-2*d)/2) # the maximum number of triplets
+        innert <- function(t) {
+          s <- n-3*q-2*d-2*t
+          cards <- 2*s+3*t+4*d+4*q
+          p <- (-1)^(n+1)
+          p <- p * choose(ranks, s)
+          p <- p * choose(ranks-s, t)
+          p <- p * choose(ranks-s-t, d)
+          p <- p * choose(ranks-s-t-d, q)
+          p <- p * 12^s
+          p <- p * 24^t
+          p <- p * 12^d
+          p <- p * 24^q
+          p <- p / prod((4*ranks-cards+1):(4*ranks))
+          p <- p * factorial(k-n) / factorial(k-cards)
+          return (p)
+        }
+        return (sum(sapply(t.min:t.max, innert)))
+      }
+      return (sum(sapply(d.min:d.max, innerd)))
+    }
+    return (sum(sapply(q.min:q.max, innerq)))
+  }
+  n.max <- floor(3*k/4)
+  p <- if (n.max > 0) sum(sapply(1:n.max, outer)) else 0
+  return (p)
 }
 
 
