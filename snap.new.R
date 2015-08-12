41603a2b4995660f91be60099efef8373689686d
Angus MacCuish <angus.maccuish@gmail.com>
2015-08-12 20:26:05 +0100

    New generic solution

---
+++ snap.new.R
@@ -0,0 +1,114 @@
+with.jokers <- function(fn) {
+  function(ranks, suits, jokers, pairs, cards, blocks=c(suits)) {
+    block.size <- sum(blocks)
+    joker.blocks <- {
+      can.accomodate.jokers <- (pairs >= block.size-1 && jokers >= block.size && cards >= block.size && block.size > 1)
+      max.pairs.possible.with.non.jokers <- ranks*(block.size-1)
+      min.joker.blocks <- (can.accomodate.jokers && max.pairs.possible.with.non.jokers < pairs)
+      max.joker.blocks <- if (can.accomodate.jokers) floor(min(jokers, cards)/block.size) else 0
+      Filter(function(j) { pairs >= j*(block.size-1) }, min.joker.blocks:max.joker.blocks)
+    } 
+    results <- lapply(joker.blocks, function(j) {
+      jokers.used <- j*block.size
+      joker.pairs <- j*(block.size-1)
+      lapply(fn(ranks, suits, jokers-jokers.used, pairs-joker.pairs, cards-jokers.used, blocks), function(x) {
+        c(x[1] * prod((jokers-block.size+1):jokers)^j, x[2] + jokers.used)
+      })
+    })
+    unlist(results, recursive=FALSE)
+  }
+}
+
+
+permutations <- function(ranks, suits, ranks.used, blocks) {
+  block.cards <- sum(blocks)
+  if (length(blocks) == 1)
+    choose(ranks, ranks.used) * prod((suits-block.cards+1):suits)^ranks.used
+  else
+    choose(ranks, ranks.used) * 12^ranks.used # hard code 2 pair (2,2) case for now
+}
+
+
+next.blocks <- function(blocks) {
+  block.cards <- sum(blocks)
+  if (length(blocks) > 1 || block.cards < 4)
+    c(block.cards-1) # either single block of 3 cards or less, or a collection of blocks
+  else # hard-code our only supported case so far: a 4 card monolithic block (4) is followed by 2 pairs (2,2)
+    if (block.cards == 4) c(2,2) else stop("Don't support more than 4 suits right now!")
+}
+
+
+f <- with.jokers(function(ranks, suits, jokers, pairs, cards, blocks=c(suits)) {
+  block.cards <- sum(blocks)
+  if (block.cards == 1) 
+    list(c(1,0))
+  else {
+    block.pairs <- sum(sapply(blocks, function(n) (n-1)))
+    min.blocks <- max(0, pairs-ranks*(block.pairs-1))
+    max.blocks <- ranks
+    n = Filter(function(n) (pairs >= n*block.pairs && n*block.cards <= cards), min.blocks:max.blocks)
+    results <- lapply(n, function(n) {
+	  pairs.created <- n*block.pairs
+	  cards.used <- n*block.cards
+      others <- f(ranks-n, suits, jokers, pairs-pairs.created, cards-cards.used, next.blocks(blocks))
+      lapply(others, function(x) c(x[1] * permutations(ranks, suits, n, blocks), x[2] + cards.used))
+    })
+    unlist(results, recursive=FALSE)
+  }
+})
+
+
+#
+#  Calculate the probability of a pair occurring in the first k cards
+#  k - the depth
+#
+pair.probability <- function(k, ranks, suits, jokers=0) {
+  deck <- ranks*suits + jokers
+  at.least.p.pairs <- function(p) {
+    probability <- function(tuple) {
+      pair.permutations <- tuple[1]
+      cards.used <- tuple[2]
+      pair.permutations * prod((k-cards.used+1):(k-p)) / prod((deck-cards.used+1):deck)
+    }
+    perms <- f(ranks, suits, jokers, p, k)
+    sum(sapply(perms, probability))
+  }
+
+  # Use the inclusion-exclusion principle to calculate the probability of a pair existing
+  max.pairs <- floor((suits-1)*k/suits)
+  if (max.pairs > 0) sum(sapply(1:max.pairs, function(p) ((-1)^(p+1) * at.least.p.pairs(p)))) else 0
+}
+
+
+#
+#  Calculate the probability of the first pair occurring at k
+#  k - the location of the first pair
+#
+first.pair.probability <- function(k, ranks, suits) {
+  if (k < 2)
+    0
+  else {
+    # m is the number of cards making up the given pair at k (ie 2 if first pair in pack)
+    # When called recursively, this function returns the probability that a pair precedes all the
+    # pairs in a block of m cards of the same suit, where the block terminates at k.
+    probability.of.pair.before <- function(k, ranks, suits, m=2) {
+      if (k <= m)
+        0
+      else if (m == suits)
+        pair.probability(k-suits, ranks-1, suits)
+      else
+        pair.probability(k-m, ranks-1, suits, suits-m) + 
+          (1-probability.of.pair.before(k, ranks, suits, m+1))*(suits-m)/(ranks*suits-m)
+    }
+    (1-probability.of.pair.before(k, ranks, suits))*(suits-1)/(ranks*suits-1)
+  }
+}
+
+
+#
+#  Calculate the mean location of the first pair
+#
+first.pair.mean.location <- function(ranks, suits) {
+  k <- 1:(ranks*suits)
+  return (sum(k * sapply(k, function(k) { first.pair.probability(k, ranks, suits) })))	
+}
