69dd618ae9b5ef67b0486bfde64c338f4509c3c1
angusmaccuish <angus.maccuish@gmail.com>
2015-08-31 10:55:50 +0100

    Bug fix, looking good for more that 5 suits\!

--- snap.R
+++ snap.R
@@ -2,23 +2,39 @@
 # and add pair contributions from the jokers. This will consequently reduce the number of normal pairs
 # to otherwise find, and increase the number of cards consumed.
 with.joker.pairs <- function(fn) {
+	
+  all.joker.blocks <- function(jokers) {
+	if (sum(jokers) > 2)
+      c(list(jokers), all.joker.blocks(next.blocks(jokers)))
+    else if (sum(jokers) == 2)
+      c(list(2), list(0))
+    else
+	  list(0)
+  }
+  
+  joker.blocks.allowed <- function(ranks, suits, pairs, cards) {
+    function(j) {
+      joker.cards <- sum(j)
+	  joker.pairs <- if (joker.cards > 0) sum(j-1) else 0
+	  joker.pairs <= pairs && joker.cards <= cards && (pairs-joker.pairs) <= floor((suits-1)*min(ranks*suits, cards-joker.cards)/suits)
+    }
+  }
+
   function(ranks, suits, jokers, pairs, cards, blocks) {
-    block.size <- sum(blocks)
-    joker.blocks <- {
-      can.accomodate.jokers <- (pairs >= block.size-1 && jokers >= block.size && cards >= block.size && block.size > 1)
-      max.pairs.possible.with.non.jokers <- ranks*(block.size-1)
-      min.joker.blocks <- (can.accomodate.jokers && max.pairs.possible.with.non.jokers < pairs)
-      max.joker.blocks <- if (can.accomodate.jokers) floor(min(jokers, cards)/block.size) else 0
-      Filter(function(j) { pairs >= j*(block.size-1) }, min.joker.blocks:max.joker.blocks)
-    } 
-    results <- lapply(joker.blocks, function(j) {
-      jokers.used <- j*block.size
-      joker.pairs <- j*(block.size-1)
-      lapply(fn(ranks, suits, jokers-jokers.used, pairs-joker.pairs, cards-jokers.used, blocks), function(x) {
-        c(x[1] * prod((jokers-block.size+1):jokers)^j, x[2] + jokers.used)
+    if (jokers < 2)
+      fn(ranks, suits, jokers, pairs, cards, blocks) # cannot make joker pairs
+    else {
+      joker.blocks <- Filter(joker.blocks.allowed(ranks, suits, pairs, cards), all.joker.blocks(jokers))
+      results <- lapply(joker.blocks, function(j) {
+        jokers.used <- sum(j)
+        joker.pairs <- if (jokers.used > 0) sum(j-1) else 0
+ 	    joker.perms <- block.permutations(1, jokers, jokers.used > 0, j)
+        results <- lapply(fn(ranks, suits, jokers-jokers.used, pairs-joker.pairs, cards-jokers.used, blocks), function(x) {
+          c(x[1] * joker.perms, x[2] + jokers.used)
+        })
       })
-    })
-    unlist(results, recursive=FALSE)
+	  unlist(results, recursive=FALSE)
+    }
   }
 }
 
@@ -45,7 +61,7 @@ next.blocks <- function(blocks) {
   # If total cards < 4, we can't split into separate groups of pairs, already atomic.
   # If no cards are > 2, or only one is and it's 3, we cannot split further.
   # If we cannot split further, jump down to the next group size.
-  if (total < 4 || length(bigger.than.two) == 0 || (length(bigger.than.two) == 1 && bigger.than.two[1] == 3)) 
+  if (total < 4 || length(bigger.than.two) == 0 || (length(bigger.than.two) == 1 && bigger.than.two[1] == 3))
     total - 1
   else {
     # Strategy: try and split into 2 halves first, then reduce size of first group until it terminates at 2.
@@ -53,9 +69,9 @@ next.blocks <- function(blocks) {
     spread <- function(blocks) {
       total <- sum(blocks)
       count <- length(blocks)
-      if (count == 1) 
+      if (count == 1)
         c(floor(total/2), total - floor(total/2))
-      else if (blocks[1] == 2) 
+      else if (blocks[1] == 2)
         c(2, spread(blocks[2:count]))
       else {
         blocks[1] <- blocks[1] - 1
@@ -68,9 +84,9 @@ next.blocks <- function(blocks) {
 }
 
 
-permutations <- with.joker.pairs(function(ranks, suits, jokers, pairs, cards, blocks) {
+permutations <- function(ranks, suits, jokers, pairs, cards, blocks) {
   block.cards <- sum(blocks)
-  if (block.cards == 1) 
+  if (block.cards == 1)
     list(c(1,0))
   else {
     block.pairs <- sum(blocks-1)
@@ -85,7 +101,7 @@ permutations <- with.joker.pairs(function(ranks, suits, jokers, pairs, cards, bl
     })
     unlist(results, recursive=FALSE)
   }
-})
+}
 
 
 #
@@ -100,7 +116,8 @@ pair.probability <- function(k, ranks, suits, jokers=0) {
       cards.used <- tuple[2]
       pair.permutations * prod((k-cards.used+1):(k-p)) / prod((deck-cards.used+1):deck)
     }
-    perms <- permutations(ranks, suits, jokers, p, k, blocks=min(suits, k))
+	fn <- with.joker.pairs(permutations)
+    perms <- fn(ranks, suits, jokers, p, k, blocks=min(suits, k))
     sum(sapply(perms, probability))
   }
 
@@ -127,7 +144,7 @@ first.pair.probability <- function(k, ranks, suits) {
       else if (m == suits)
         pair.probability(k-suits, ranks-1, suits)
       else
-        pair.probability(k-m, ranks-1, suits, suits-m) + 
+        pair.probability(k-m, ranks-1, suits, suits-m) +
           (1-probability.of.pair.before(k, ranks, suits, m+1))*(suits-m)/(ranks*suits-m)
     }
     (1-probability.of.pair.before(k, ranks, suits))*(suits-1)/(ranks*suits-1)
@@ -140,7 +157,7 @@ first.pair.probability <- function(k, ranks, suits) {
 #
 first.pair.mean.location <- function(ranks, suits) {
   k <- 1:(ranks*suits)
-  return (sum(k * sapply(k, function(k) first.pair.probability(k, ranks, suits))))	
+  return (sum(k * sapply(k, function(k) first.pair.probability(k, ranks, suits))))
 }
 
 
